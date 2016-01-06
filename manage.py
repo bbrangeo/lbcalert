f70d2dd3d82fb8018760f145f152aba11e746593
Koding <aimon.nicolas@gmail.com>
2016-01-06 09:34:05 +0000

    Revert "imports back in app.py"
    This reverts commit c78958fe21a63e05d8d86ca0d5f563a13ee4bd2e.

--- manage.py
+++ manage.py
@@ -3,6 +3,10 @@ from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 from app import app, db
+import models
+from parser import parselbc
+import scheduler
+import router
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
