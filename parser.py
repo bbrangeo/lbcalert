1643b99b4b2e2e459649b25a63096f811b88e55e
Koding <aimon.nicolas@gmail.com>
2016-01-06 18:24:28 +0000

    send issue emails to me

--- parser.py
+++ parser.py
@@ -26,7 +26,7 @@ def parselbc(id):
             links = lbclist.findAll("a")
         except:
             mail=Mail(app)
-            msg = Message('[LBCbot] Problem parsing "'+search.title+'"', sender='lbcbot@gmail.com', recipients=[search.email,])
+            msg = Message('[LBCbot] Problem parsing "'+search.title+'"', sender='lbcbot@gmail.com', recipients=["aimon.nicolas@gmail.com",])
             msg.html = render_template('email_parseissue.html', issue=sys.exc_info(), response = r)
             mail.send(msg)            
             return id
