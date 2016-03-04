c969366b6287336146f25563eebf76724e77365a
naimo <aimon.nicolas@gmail.com>
2016-03-04 08:24:34 +0000

    more setup

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
-# #!/bin/bash
+#!/bin/bash
 
-pip install virtualenv
-pip install virtualenvwrapper
+sudo pip install --upgrade virtualenv
+sudo pip install --upgrade virtualenvwrapper
 
 echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
 echo 'export PROJECT_HOME=$HOME/Devel' >> ~/.bashrc
 echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bashrc
 
-mkvirtualenv --python=/usr/bin/python3 lbcalert
-pip install -r requirements.txt
+source ~/.bashrc
 
-echo 'export APP_SETTINGS="config.DevelopmentConfig"'  ~/.virtualenvs/lbcalert/postactivate
-echo 'export DATABASE_URL="postgresql://localhost/wordcount_dev"'  ~/.virtualenvs/lbcalert/postactivate
+mkvirtualenv -p python3 ~/.virtualenvs/lbcalert
+echo 'export APP_SETTINGS="config.DevelopmentConfig"' >> ~/.virtualenvs/lbcalert/bin/postactivate
+echo 'export DATABASE_URL="postgresql://localhost/lbcalert_dev"' >>  ~/.virtualenvs/lbcalert/bin/postactivate
+
+workon lbcalert
+
+pip install -r requirements.txt
